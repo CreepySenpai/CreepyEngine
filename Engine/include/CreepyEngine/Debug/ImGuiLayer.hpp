88b8742a1c3a9f5404aaa58ca2972ed550ce6376
CreepySenpai <tuana8255@gmail.com>
2024-03-15 14:46:19 +0700

    Add Editor Config

--- Engine/include/CreepyEngine/Debug/ImGuiLayer.hpp
+++ Engine/include/CreepyEngine/Debug/ImGuiLayer.hpp
@@ -7,6 +7,8 @@
 #include <CreepyEngine/Events/KeyEvent.hpp>
 #include <GLFW/glfw3.h>
 #include <imgui/imgui.h>
+#include <glm/glm.hpp>
+#include <memory>
 
 namespace Creepy
 {
@@ -18,6 +20,7 @@ namespace Creepy
 
             void OnAttach() noexcept override;
             void OnDetach() noexcept override;
+            void OnUpdate(TimeStep timeStep) noexcept override;
             void OnImGuiRender() noexcept override;
             void OnEvent(Event& event) noexcept override;
 
@@ -33,6 +36,34 @@ namespace Creepy
         private:
             float m_time{0.0f};
             bool m_blockEvents{true};
+
+            struct LayerConfig{
+                glm::vec4 WindowBg{0.1f, 0.105f, 0.11f, 1.0f};
+
+                glm::vec4 Header{0.2f, 0.205f, 0.21f, 1.0f};
+                glm::vec4 HeaderHovered{0.3f, 0.305f, 0.31f, 1.0f};
+                glm::vec4 HeaderActive{0.15f, 0.1505f, 0.151f, 1.0f};
+
+                glm::vec4 Button{0.2f, 0.205f, 0.21f, 1.0f};
+                glm::vec4 ButtonHovered{ 0.3f, 0.305f, 0.31f, 1.0f};
+                glm::vec4 ButtonActive{0.15f, 0.1505f, 0.151f, 1.0f};
+
+                glm::vec4 FrameBg{0.2f, 0.205f, 0.21f, 1.0f};
+                glm::vec4 FrameBgHovered{0.3f, 0.305f, 0.31f, 1.0f};
+                glm::vec4 FrameBgActive{0.15f, 0.1505f, 0.151f, 1.0f};
+
+                glm::vec4 Tab{0.15f, 0.1505f, 0.151f, 1.0f};
+                glm::vec4 TabHovered{0.38f, 0.3805f, 0.381f, 1.0f};
+                glm::vec4 TabActive{0.28f, 0.2805f, 0.281f, 1.0f};
+                glm::vec4 TabUnfocused{0.15f, 0.1505f, 0.151f, 1.0f};
+                glm::vec4 TabUnfocusedActive{0.2f, 0.205f, 0.21f, 1.0f};
+
+                glm::vec4 TitleBg{0.15f, 0.1505f, 0.151f, 1.0f};
+                glm::vec4 TitleBgActive{0.15f, 0.1505f, 0.151f, 1.0f};
+                glm::vec4 TitleBgCollapse{0.15f, 0.1505f, 0.151f, 1.0f};
+            };
+
+            LayerConfig m_editorConfig{};
     };
 
 }
--- Engine/src/CreepyEngine/Debug/ImGuiLayer.cpp
+++ Engine/src/CreepyEngine/Debug/ImGuiLayer.cpp
@@ -6,7 +6,6 @@
 
 namespace Creepy
 {
-
     ImGuiLayer::ImGuiLayer() noexcept : Layer("ImGuiLayer") {
 
     }
@@ -49,8 +48,34 @@ namespace Creepy
         ImGui::DestroyContext();
     }
 
-    void ImGuiLayer::OnImGuiRender() noexcept {    
-        
+    void ImGuiLayer::OnUpdate(TimeStep timeStep) noexcept {
+        this->SetTheme();
+    }
+
+    void ImGuiLayer::OnImGuiRender() noexcept {   
+        ImGui::Begin("Theme Setting");
+
+            ImGui::ColorEdit4("Window Background", glm::value_ptr(m_editorConfig.WindowBg));
+            ImGui::ColorEdit4("Header", glm::value_ptr(m_editorConfig.Header));
+            ImGui::ColorEdit4("Header Hovered", glm::value_ptr(m_editorConfig.HeaderHovered));
+            ImGui::ColorEdit4("Header Active", glm::value_ptr(m_editorConfig.HeaderActive));
+
+            ImGui::ColorEdit4("Button", glm::value_ptr(m_editorConfig.Button));
+            ImGui::ColorEdit4("Button Hovered", glm::value_ptr(m_editorConfig.ButtonHovered));
+            ImGui::ColorEdit4("Button Active", glm::value_ptr(m_editorConfig.ButtonActive));
+
+            ImGui::ColorEdit4("Frame Background", glm::value_ptr(m_editorConfig.FrameBg));
+            ImGui::ColorEdit4("FrameBg Hovered", glm::value_ptr(m_editorConfig.FrameBgHovered));
+            ImGui::ColorEdit4("FrameBg Active", glm::value_ptr(m_editorConfig.FrameBgActive));
+
+            ImGui::ColorEdit4("Tab", glm::value_ptr(m_editorConfig.Tab));
+            ImGui::ColorEdit4("Tab Hovered", glm::value_ptr(m_editorConfig.TabHovered));
+            ImGui::ColorEdit4("Tab Active", glm::value_ptr(m_editorConfig.TabActive));
+            ImGui::ColorEdit4("Tab Unfocused", glm::value_ptr(m_editorConfig.TabUnfocused));
+            ImGui::ColorEdit4("Tab Unfocused Active", glm::value_ptr(m_editorConfig.TabUnfocusedActive));
+
+
+            ImGui::End();
     }
 
     void ImGuiLayer::Begin() noexcept {
@@ -92,46 +117,38 @@ namespace Creepy
 
     void ImGuiLayer::SetTheme() noexcept {
         auto& colors = ImGui::GetStyle().Colors;
-
+        
         //Background
-        colors[ImGuiCol_WindowBg] = ImVec4{ 0.1f, 0.105f, 0.11f, 1.0f };
+        colors[ImGuiCol_WindowBg] = ImVec4{m_editorConfig.WindowBg.r, m_editorConfig.WindowBg.g, m_editorConfig.WindowBg.b, m_editorConfig.WindowBg.a};
 
 		// Headers
-		colors[ImGuiCol_Header] = ImVec4{ 0.2f, 0.205f, 0.21f, 1.0f };
-		colors[ImGuiCol_HeaderHovered] = ImVec4{ 0.3f, 0.305f, 0.31f, 1.0f };
-		colors[ImGuiCol_HeaderActive] = ImVec4{ 0.15f, 0.1505f, 0.151f, 1.0f };
+		colors[ImGuiCol_Header] = ImVec4{m_editorConfig.Header.r, m_editorConfig.Header.g, m_editorConfig.Header.b, m_editorConfig.Header.a};
 
-        // colors[ImGuiCol_Header] = ImVec4{ 230.f / 255.f, 141.0f / 255.0f, 224.0f / 255.f, 1.0f };
-		// colors[ImGuiCol_HeaderHovered] = ImVec4{ 230.f / 255.f, 141.0f / 255.0f, 224.0f / 255.f, 1.0f };
-		// colors[ImGuiCol_HeaderActive] = ImVec4{ 230.f / 255.f, 141.0f / 255.0f, 224.0f / 255.f, 1.0f };
+		colors[ImGuiCol_HeaderHovered] = ImVec4{m_editorConfig.HeaderHovered.r, m_editorConfig.HeaderHovered.g, m_editorConfig.HeaderHovered.b, m_editorConfig.HeaderHovered.a};
+		colors[ImGuiCol_HeaderActive] = ImVec4{ m_editorConfig.HeaderActive.r, m_editorConfig.HeaderActive.g, m_editorConfig.HeaderActive.b, m_editorConfig.HeaderActive.a};
 		
+        ENGINE_LOG_WARNING("Call change button color: R:{}, G:{}, B:{}, A:{}", m_editorConfig.Button.r, m_editorConfig.Button.g, m_editorConfig.Button.b, m_editorConfig.Button.a);
 		// Buttons
-		colors[ImGuiCol_Button] = ImVec4{ 0.2f, 0.205f, 0.21f, 1.0f };
-		colors[ImGuiCol_ButtonHovered] = ImVec4{ 0.3f, 0.305f, 0.31f, 1.0f };
-		colors[ImGuiCol_ButtonActive] = ImVec4{ 0.15f, 0.1505f, 0.151f, 1.0f };
+		colors[ImGuiCol_Button] = ImVec4{ m_editorConfig.Button.r, m_editorConfig.Button.g, m_editorConfig.Button.b, m_editorConfig.Button.a };
+		colors[ImGuiCol_ButtonHovered] = ImVec4{ m_editorConfig.ButtonHovered.r, m_editorConfig.ButtonHovered.g, m_editorConfig.ButtonHovered.b, m_editorConfig.ButtonHovered.a };
+		colors[ImGuiCol_ButtonActive] = ImVec4{ m_editorConfig.ButtonActive.r, m_editorConfig.ButtonActive.g, m_editorConfig.ButtonActive.b, m_editorConfig.ButtonActive.a };
 
 		// Frame BG
-		colors[ImGuiCol_FrameBg] = ImVec4{ 0.2f, 0.205f, 0.21f, 1.0f };
-		colors[ImGuiCol_FrameBgHovered] = ImVec4{ 0.3f, 0.305f, 0.31f, 1.0f };
-		colors[ImGuiCol_FrameBgActive] = ImVec4{ 0.15f, 0.1505f, 0.151f, 1.0f };
+		colors[ImGuiCol_FrameBg] = ImVec4{ m_editorConfig.FrameBg.r, m_editorConfig.FrameBg.g, m_editorConfig.FrameBg.b, m_editorConfig.FrameBg.a  };
+		colors[ImGuiCol_FrameBgHovered] = ImVec4{ m_editorConfig.FrameBgHovered.r, m_editorConfig.FrameBgHovered.g, m_editorConfig.FrameBgHovered.b, m_editorConfig.FrameBgHovered.a  };
+		colors[ImGuiCol_FrameBgActive] = ImVec4{ m_editorConfig.FrameBgActive.r, m_editorConfig.FrameBgActive.g, m_editorConfig.FrameBgActive.b, m_editorConfig.FrameBgActive.a  };
 
 		// Tabs
-		// colors[ImGuiCol_Tab] = ImVec4{ 0.15f, 0.1505f, 0.151f, 1.0f };
-		// colors[ImGuiCol_TabHovered] = ImVec4{ 0.38f, 0.3805f, 0.381f, 1.0f };
-		// colors[ImGuiCol_TabActive] = ImVec4{ 0.28f, 0.2805f, 0.281f, 1.0f };
-		// colors[ImGuiCol_TabUnfocused] = ImVec4{ 0.15f, 0.1505f, 0.151f, 1.0f };
-		// colors[ImGuiCol_TabUnfocusedActive] = ImVec4{ 0.2f, 0.205f, 0.21f, 1.0f };
-
-        colors[ImGuiCol_Tab] = ImVec4{ 230.f / 255.f, 141.0f / 255.0f, 224.0f / 255.f, 1.0f };
-		colors[ImGuiCol_TabHovered] = ImVec4{ 230.f / 255.f, 141.0f / 255.0f, 224.0f / 255.f, 1.0f };
-		colors[ImGuiCol_TabActive] = ImVec4{ 230.f / 255.f, 141.0f / 255.0f, 224.0f / 255.f, 1.0f };
-		colors[ImGuiCol_TabUnfocused] = ImVec4{ 230.f / 255.f, 141.0f / 255.0f, 224.0f / 255.f, 1.0f };
-		colors[ImGuiCol_TabUnfocusedActive] = ImVec4{ 230.f / 255.f, 141.0f / 255.0f, 224.0f / 255.f, 1.0f };
+		colors[ImGuiCol_Tab] = ImVec4{ m_editorConfig.Tab.r, m_editorConfig.Tab.g, m_editorConfig.Tab.b, m_editorConfig.Tab.a };
+		colors[ImGuiCol_TabHovered] = ImVec4{ m_editorConfig.TabHovered.r, m_editorConfig.TabHovered.g, m_editorConfig.TabHovered.b, m_editorConfig.TabHovered.a };
+		colors[ImGuiCol_TabActive] = ImVec4{ m_editorConfig.TabActive.r, m_editorConfig.TabActive.g, m_editorConfig.TabActive.b, m_editorConfig.TabActive.a };
+		colors[ImGuiCol_TabUnfocused] = ImVec4{ m_editorConfig.TabUnfocused.r, m_editorConfig.TabUnfocused.g, m_editorConfig.TabUnfocused.b, m_editorConfig.TabUnfocused.a };
+		colors[ImGuiCol_TabUnfocusedActive] = ImVec4{ m_editorConfig.TabUnfocusedActive.r, m_editorConfig.TabUnfocusedActive.g, m_editorConfig.TabUnfocusedActive.b, m_editorConfig.TabUnfocusedActive.a  };
 
 		// Title
-		colors[ImGuiCol_TitleBg] = ImVec4{ 0.15f, 0.1505f, 0.151f, 1.0f };
-		colors[ImGuiCol_TitleBgActive] = ImVec4{ 0.15f, 0.1505f, 0.151f, 1.0f };
-		colors[ImGuiCol_TitleBgCollapsed] = ImVec4{ 0.15f, 0.1505f, 0.151f, 1.0f };
+		colors[ImGuiCol_TitleBg] = ImVec4{ m_editorConfig.TitleBg.r, m_editorConfig.TitleBg.g, m_editorConfig.TitleBg.b, m_editorConfig.TitleBg.a };
+		colors[ImGuiCol_TitleBgActive] = ImVec4{  m_editorConfig.TitleBgActive.r, m_editorConfig.TitleBgActive.g, m_editorConfig.TitleBgActive.b, m_editorConfig.TitleBgActive.a };
+		colors[ImGuiCol_TitleBgCollapsed] = ImVec4{ m_editorConfig.TitleBgCollapse.r, m_editorConfig.TitleBgCollapse.g, m_editorConfig.TitleBgCollapse.b, m_editorConfig.TitleBgCollapse.a };
 
     }
 }
